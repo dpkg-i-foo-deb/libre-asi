a553112f72544e0eb68e107b2e7ee65f67e15344
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-09 11:07:43 -0500

    Protect index route

--- api/routes/index_routes.go
+++ api/routes/index_routes.go
@@ -2,9 +2,10 @@ package routes
 
 import (
 	"libre-asi-api/app"
+	"libre-asi-api/auth"
 	"libre-asi-api/services"
 )
 
 func indexRoute() {
-	app.AddGet("/", services.IndexService)
+	app.AddGet("/", auth.ValidateAndContinue, services.IndexService)
 }
