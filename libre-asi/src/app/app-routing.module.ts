a7d8dcafee65bbd69ba48d308569b267d1e93d3f
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-09 12:01:34 -0500

    Add AuthGuard to home module

--- libre-asi/src/app/app-routing.module.ts
+++ libre-asi/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/core/guards/auth.guard';
 import { HomeComponent } from 'src/home/view/home.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
