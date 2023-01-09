d35549b8c4baee6739cca59aa1bfc3a91669aeee
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-09 16:46:42 -0500

    Implement Angular Material

--- libre-asi/src/app/app.module.ts
+++ libre-asi/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { LoginModule } from 'src/login/login.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     CoreModule,
     HomeModule,
     LoginModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
