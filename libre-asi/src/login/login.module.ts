3a9f994997c4681ec255fee9c4cf6b0a84f9ee78
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-09 19:05:59 -0500

    Continue with the login thingy

--- libre-asi/src/login/login.module.ts
+++ libre-asi/src/login/login.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './view/login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatRadioModule,
+  ],
   exports: [LoginComponent],
 })
 export class LoginModule {}
--- libre-asi/src/login/view/login.component.ts
+++ libre-asi/src/login/view/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
 }
