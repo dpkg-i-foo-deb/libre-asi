a8e88b6995714fded5aa4008c9637fb1e6adf42d
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-12 09:37:29 -0500

    Protect register route

--- api/routes/register_route.go
+++ api/routes/register_route.go
@@ -2,9 +2,10 @@ package routes
 
 import (
 	"libre-asi-api/app"
+	"libre-asi-api/auth"
 	"libre-asi-api/services"
 )
 
 func registerRoute() {
-	app.AddPost("/register/:role", services.RegisterService)
+	app.AddPost("/register/:role", auth.ValidateAndContinue, services.RegisterService)
 }
