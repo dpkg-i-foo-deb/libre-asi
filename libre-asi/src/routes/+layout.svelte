d81774730ed10d1813f5cdea512bf03efc4b11cf
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-16 11:26:44 -0500

    Add a sidenav

--- libre-asi/src/routes/+layout.svelte
+++ libre-asi/src/routes/+layout.svelte
@@ -1,5 +1,65 @@
 <script lang="ts">
 	import 'carbon-components-svelte/css/all.css';
+	import {
+		Header,
+		HeaderUtilities,
+		HeaderAction,
+		HeaderGlobalAction,
+		HeaderPanelLinks,
+		HeaderPanelDivider,
+		HeaderPanelLink,
+		SideNav,
+		SideNavItems,
+		SideNavLink,
+		SkipToContent,
+		Content,
+		Grid,
+		Row,
+		Column
+	} from 'carbon-components-svelte';
+	import SettingsAdjust from 'carbon-icons-svelte/lib/SettingsAdjust.svelte';
+	import UserAvatarFilledAlt from 'carbon-icons-svelte/lib/UserAvatarFilledAlt.svelte';
+
+	let isSideNavOpen = false;
+	let isOpen1 = false;
 </script>
 
-<slot />
+<Header persistentHamburgerMenu={true} bind:isSideNavOpen company="Libre-ASI">
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderUtilities>
+		<HeaderGlobalAction aria-label="Settings" icon={SettingsAdjust} />
+		<HeaderAction bind:isOpen={isOpen1} icon={UserAvatarFilledAlt} closeIcon={UserAvatarFilledAlt}>
+			<HeaderPanelLinks>
+				<HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
+				<HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+				<HeaderPanelLink>Switcher item 2</HeaderPanelLink>
+				<HeaderPanelLink>Switcher item 3</HeaderPanelLink>
+				<HeaderPanelLink>Switcher item 4</HeaderPanelLink>
+				<HeaderPanelDivider>Switcher subject 2</HeaderPanelDivider>
+				<HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+				<HeaderPanelLink>Switcher item 2</HeaderPanelLink>
+				<HeaderPanelDivider>Switcher subject 3</HeaderPanelDivider>
+				<HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+			</HeaderPanelLinks>
+		</HeaderAction>
+	</HeaderUtilities>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+	<SideNavItems>
+		<SideNavLink text="Link 1" />
+		<SideNavLink text="Link 2" />
+		<SideNavLink text="Link 3" />
+	</SideNavItems>
+</SideNav>
+<Content>
+	<Grid>
+		<Row>
+			<Column>
+				<slot />
+			</Column>
+		</Row>
+	</Grid>
+</Content>
--- libre-asi/svelte.config.js
+++ libre-asi/svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), optimizeImports()],
 
 	kit: {
 		adapter: adapter({ out: 'build', fallback: 'index.html' }),
