6b8d4897bed54fb8d58f56c910d48e9198fb45fe
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-17 14:22:59 -0500

    Get administrators service!

--- api/routes/init.go
+++ api/routes/init.go
@@ -7,4 +7,5 @@ func init() {
 	signOutRoute()
 	worldRoute()
 	refreshRoute()
+	adminRoutes()
 }
---
+++ api/services/administrator_services.go
@@ -0,0 +1,25 @@
+package services
+
+import (
+	"libre-asi-api/database"
+	"libre-asi-api/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetAdministratorsService(c *fiber.Ctx) error {
+
+	var res models.Response
+	res.Status = string(models.STATUS_ERROR)
+	res.Message = "Something went wrong"
+
+	var admins []models.Administrator
+
+	result := database.DB.Find(&admins)
+
+	if result.Error != nil {
+		return c.Status(500).JSON(res)
+	}
+
+	return c.Status(200).JSON(admins)
+}
