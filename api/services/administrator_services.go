27aade02f66c93775b794c0dc9d0d19999dadec1
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-17 14:52:34 -0500

    Improve get admins service

--- api/services/administrator_services.go
+++ api/services/administrator_services.go
@@ -13,13 +13,25 @@ func GetAdministratorsService(c *fiber.Ctx) error {
 	res.Status = string(models.STATUS_ERROR)
 	res.Message = "Something went wrong"
 
-	var admins []models.User
+	var admins []models.Administrator
+	var ids []uint
+	var users []models.User
 
-	result := database.DB.Omit("people", "password").Preload("Administrators").Find(&admins)
+	result := database.DB.Find(&admins)
 
 	if result.Error != nil {
 		return c.Status(500).JSON(res)
 	}
 
-	return c.Status(200).JSON(admins)
+	for i := range admins {
+		ids = append(ids, admins[i].UserID)
+	}
+
+	result = database.DB.Omit("password", "administrators", "people").Find(&users, ids)
+
+	if result.Error != nil {
+		return c.Status(500).JSON(res)
+	}
+
+	return c.Status(200).JSON(&users)
 }
