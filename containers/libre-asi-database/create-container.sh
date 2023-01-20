a1c7c767261fa0c735e4a38ac4a7c822c045e47d
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-20 14:19:10 -0500

    Remove some stuff from the database container

--- containers/libre-asi-database/create-container.sh
+++ containers/libre-asi-database/create-container.sh
@@ -13,8 +13,4 @@ podman cp ../../database/scripts/0-create-user-database.sql libre-asi-database:/
 
 podman exec -it libre-asi-database psql -U postgres -f 0-create-user-database.sql
 
-podman cp ../../database/scripts/1-create-database.sql libre-asi-database:/1-create-database.sql
-
-podman exec -it libre-asi-database psql -U libre_asi -d libre_asi -f 1-create-database.sql
-
 #More stuffies will be added here later
