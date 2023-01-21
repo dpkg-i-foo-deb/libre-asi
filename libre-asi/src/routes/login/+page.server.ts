3475a909aca583d2c33526b7aa556bcef0f2f48b
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-20 19:43:49 -0500

    Make a kinda functional login

--- libre-asi/src/routes/login/+page.server.ts
+++ libre-asi/src/routes/login/+page.server.ts
@@ -4,20 +4,16 @@ import type User from '$lib/models/User';
 import { fail, redirect } from '@sveltejs/kit';
 import type { Action, Actions, PageServerLoad } from './$types';
 
-const login: Action = async function ({ cookies, request, fetch, }) {
+const login: Action = async function ({ cookies, request, fetch }) {
 	const data = await request.formData();
+	let response: Response;
 
 	const email = data.get('email');
 	const password = data.get('password');
 	const administrator = data.get('administrator');
 	const interviewer = data.get('interviewer');
 
-	console.log(administrator);
-	console.log(interviewer);
-
 	const fullUrl = `${apiUrl}login/${administrator ?? interviewer}`;
-
-	console.log(fullUrl);
 
 	if (typeof email !== 'string' || typeof password !== 'string') {
 		return fail(400, { badRequest: true });
@@ -25,10 +21,8 @@ const login: Action = async function ({ cookies, request, fetch, }) {
 
 	const user: User = { email: email, password: password };
 
-	console.log(user);
-
 	try {
-		const response = await fetch(fullUrl, {
+		response = await fetch(fullUrl, {
 			mode: 'cors',
 			method: 'POST',
 			credentials: 'include',
@@ -38,19 +32,29 @@ const login: Action = async function ({ cookies, request, fetch, }) {
 		if (response.status == 401) {
 			return fail(401, { invalidCredentials: true });
 		}
-
-		if (response.status == 200) {
-
-			const pair : JwtPair = await response.json()
-
-			console.log(pair)
-
-			return {status:200, headers:'owo'}
-
-		}
 	} catch (e) {
 		console.log(e);
 		return fail(500, { cannotConnect: true });
+	}
+
+	if (response.status == 200) {
+		const pair: JwtPair = await response.json();
+
+		cookies.set(pair.access_token.name, pair.access_token.value, {
+			httpOnly: pair.access_token.http_only,
+			expires: new Date(pair.access_token.expires),
+			path: pair.access_token.path,
+			secure: pair.access_token.secure
+		});
+
+		cookies.set(pair.refresh_token.name, pair.refresh_token.value, {
+			httpOnly: pair.refresh_token.http_only,
+			expires: new Date(pair.refresh_token.expires),
+			path: pair.refresh_token.path,
+			secure: pair.refresh_token.secure
+		});
+
+		throw redirect(307, '/');
 	}
 };
 
