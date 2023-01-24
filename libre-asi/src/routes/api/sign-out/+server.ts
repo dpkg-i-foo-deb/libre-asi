352817bd0746830cd17d40c170bff76184960217
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-24 14:11:05 -0500

    Experimenting with response options

--- libre-asi/src/routes/api/sign-out/+server.ts
+++ libre-asi/src/routes/api/sign-out/+server.ts
@@ -1,8 +1,15 @@
 import { apiUrl, signOut as signOutRoute } from '$lib/api/constants';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const POST: RequestHandler = async () => {
-	const response = fetch(apiUrl + signOutRoute, { method: 'POST' });
+export const POST: RequestHandler = async ({ fetch }) => {
+	const body = 'fail';
+	const failOptions = { status: 500, statusText: 'Something went wrong' };
 
-	return response;
+	let response: Response;
+	try {
+		response = await fetch(apiUrl + signOutRoute, { method: 'POST' });
+		return response;
+	} catch (e) {}
+
+	return new Response(body, failOptions);
 };
