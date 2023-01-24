2d38e001d960334effc81ab5ee552ad4b5ee9fba
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-24 14:22:47 -0500

    Deleting cookies

--- libre-asi/src/routes/api/sign-out/+server.ts
+++ libre-asi/src/routes/api/sign-out/+server.ts
@@ -1,15 +1,19 @@
 import { apiUrl, signOut as signOutRoute } from '$lib/api/constants';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const POST: RequestHandler = async ({ fetch }) => {
+export const POST: RequestHandler = async ({ fetch, cookies }) => {
 	const body = 'fail';
 	const failOptions = { status: 500, statusText: 'Something went wrong' };
 
 	let response: Response;
 	try {
 		response = await fetch(apiUrl + signOutRoute, { method: 'POST' });
-		return response;
-	} catch (e) {}
 
-	return new Response(body, failOptions);
+		cookies.delete('access-token', { path: '/' });
+		cookies.delete('refresh-token', { path: '/' });
+
+		return new Response();
+	} catch (e) {
+		return new Response(body, failOptions);
+	}
 };
