4b7fa42ae1896e313597b37df7a39462e1d76806
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-25 10:13:48 -0500

    Fix management page

--- libre-asi/src/routes/management/administrators/+layout.svelte
+++ libre-asi/src/routes/management/administrators/+layout.svelte
@@ -1,20 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { session, role } from '$lib/stores/userStore';
+	import session from '$lib/stores/userStore';
 	import { goto } from '$app/navigation';
-
-	let activeSession = false;
-
-	onMount(async () => {
-		if ($session == 'false' || $role != 'admin') {
-			activeSession = false;
-			goto('/login');
-		} else {
-			activeSession = true;
-		}
-	});
+	import { SessionRole } from '$lib/models/Session';
 </script>
 
-{#if activeSession}
+{#if $session.active && $session.role == SessionRole.Admin}
 	<slot />
 {/if}
