7a5aba079806af69d1fd5f0484fcd33481087430
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-25 10:06:30 -0500

    Remove unused imports

--- libre-asi/src/routes/api/sign-out/+server.ts
+++ libre-asi/src/routes/api/sign-out/+server.ts
@@ -1,4 +1,3 @@
-import { apiUrl, signOut as signOutRoute } from '$lib/api/constants';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ fetch, cookies, request }) => {
