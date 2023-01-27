e1667ef81e036fd8d82f8e4196faa0056027fb64
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-27 12:06:06 -0500

    Fix sign-out

--- libre-asi/src/routes/api/sign-out/+server.ts
+++ libre-asi/src/routes/api/sign-out/+server.ts
@@ -4,8 +4,8 @@ export const POST: RequestHandler = async function ({ cookies }) {
 	let body = 'Success';
 	const options = { status: 200, statusText: 'Logged out' };
 
-	cookies.delete('access-token', { path: '/' });
-	cookies.delete('refresh-token', { path: '/' });
+	cookies.set('access-token', '', { path: '/' });
+	cookies.set('refresh-token', '', { path: '/' });
 
 	return new Response(body, options);
 };
