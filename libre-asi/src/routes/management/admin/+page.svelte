64c346df9f0e8d39317c59955ccb56293709777e
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-29 13:27:02 -0500

    Experimenting with modals

--- libre-asi/src/routes/management/admin/+page.svelte
+++ libre-asi/src/routes/management/admin/+page.svelte
@@ -4,7 +4,13 @@
 		DataTable,
 		Toolbar,
 		ToolbarContent,
-		ToolbarSearch
+		ToolbarSearch,
+		DataTableSkeleton,
+		ComposedModal,
+		ModalHeader,
+		ModalFooter,
+		ModalBody,
+		TextInput
 	} from 'carbon-components-svelte';
 	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
@@ -14,10 +20,11 @@
 	import { goto } from '$app/navigation';
 	import type Administrator from '$lib/models/Administrator';
 	import type { DataTableRow } from 'carbon-components-svelte/types/DataTable/DataTable.svelte';
-	import { DataTableSkeleton } from 'carbon-components-svelte';
+	import { enhance } from '$app/forms';
 	export let data: PageData;
 
 	let rows: ReadonlyArray<DataTableRow>;
+	let isOpen = false;
 
 	onMount(function () {
 		if (data.error) {
@@ -62,8 +69,54 @@
 		<Toolbar>
 			<ToolbarContent>
 				<ToolbarSearch />
-				<Button>Register Administrator</Button>
+				<Button
+					on:click={() => {
+						isOpen = true;
+					}}>Register Administrator</Button
+				>
 			</ToolbarContent>
 		</Toolbar>
 	</DataTable>
+
+	<ComposedModal bind:open={isOpen} selectorPrimaryFocus="#email">
+		<ModalHeader label="Transaction" title="New Administrator Account" />
+		<ModalBody hasForm>
+			<form>
+				<h7 class="paragraph">
+					Administrators can register other administrators, they can also trigger password resets,
+					access interviews and accounts information.
+				</h7>
+				<br />
+				<h6 class="paragraph">
+					The account password will be generated automatically and the new user will be prompted to
+					change it
+				</h6>
+				<div class="input-field">
+					<TextInput id="email" labelText="Email" placeholder="Enter email..." />
+				</div>
+				<div class="input-field">
+					<TextInput labelText="User name" placeholder="Enter user name..." />
+				</div>
+			</form>
+		</ModalBody>
+		<ModalFooter
+			primaryButtonText="Register"
+			secondaryButtonText="Cancel"
+			on:click:button--secondary={() => {
+				isOpen = false;
+			}}
+		/>
+	</ComposedModal>
 {/if}
+
+<style>
+	.input-field {
+		padding-top: 10px;
+		padding-bottom: 10px;
+	}
+
+	.paragraph {
+		padding-top: 5px;
+		padding-bottom: 5px;
+	}
+</style>
