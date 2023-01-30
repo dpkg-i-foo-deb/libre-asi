0f66fdcf8903d4e9ad3b352ed1795acf6b6ce0d9
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-30 13:07:33 -0500

    Send error notification function

--- libre-asi/src/lib/util/notifications.ts
+++ libre-asi/src/lib/util/notifications.ts
@@ -5,7 +5,18 @@ export function sendSuccess(title: string, subtitle: string) {
 		title: title,
 		subtitle: subtitle,
 		caption: new Date().toLocaleDateString(),
-		timeout: 1000,
+		timeout: 10000,
+		visible: true
+	});
+}
+
+export function sendError(title: string, subtitle: string) {
+	notifications.set({
+		kind: 'error',
+		title: title,
+		subtitle: subtitle,
+		caption: new Date().toLocaleDateString(),
+		timeout: 10000,
 		visible: true
 	});
 }
