4e860e7701b093a03639f22b16119fe2791b56a7
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-30 14:03:39 -0500

    Add cannot connect page

---
+++ libre-asi/src/routes/cannot-connect/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { ExpandableTile } from 'carbon-components-svelte';
+</script>
+
+<h1>Error.</h1>
+<h3>
+	Something went wrong while processing your request, try again later, if the error persists.
+	Contact your administrator.
+</h3>
+<h4>Code: 503.</h4>
+
+<div class="help">
+	<ExpandableTile>
+		<div slot="above" style="height:3rem">
+			If you're the administrator... Click here to see some help
+		</div>
+		<div slot="below">
+			Status code 503 is thrown when Libre-ASI API can't be reached, this can be caused by either
+			misconfiguring environment variables or stopping the API server. Make sure it is running,
+			check server logs and its address is correctly configured on the server and try again
+		</div>
+	</ExpandableTile>
+</div>
+
+<style>
+	h1 {
+		font-weight: 500;
+		margin-bottom: 15px;
+	}
+
+	h3 {
+		margin-bottom: 25px;
+	}
+
+	.help {
+		margin-top: 40px;
+	}
+</style>
