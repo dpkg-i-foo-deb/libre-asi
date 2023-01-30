bf4a7609b116c7da9e7decc5ef080541b5ccb445
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-30 14:51:31 -0500

    Redirect to error page

--- libre-asi/src/routes/management/admin/+page.server.ts
+++ libre-asi/src/routes/management/admin/+page.server.ts
@@ -1,23 +1,25 @@
 import { API_URL, GET_ADMINS } from '$lib/api/constants';
 import type Administrator from '$lib/models/Administrator';
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async function ({ fetch, request }) {
 	let response: Response;
 
-	try {
-		response = await fetch(API_URL + GET_ADMINS, {
-			credentials: 'include',
-			headers: request.headers
-		});
+	response = await fetch(API_URL + GET_ADMINS, {
+		credentials: 'include',
+		headers: request.headers
+	});
 
-		if (response.ok) {
-			const administrators: Administrator[] = await response.json();
+	if (response.ok) {
+		const administrators: Administrator[] = await response.json();
 
-			return { administrators };
-		}
-	} catch (e) {
-		console.log(e);
+		return { administrators };
 	}
+
+	if (response.status == 503) {
+		throw redirect(301, '/cannot-connect');
+	}
+
 	return { error: true };
 };
