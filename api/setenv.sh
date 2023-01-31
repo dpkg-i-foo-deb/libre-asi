b6d1d54344122e0ca4fdf31cdf5b3bc529be92c9
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-01-31 15:22:41 -0500

    Add auth key environemnt variable

--- api/setenv.sh
+++ api/setenv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export AUTH_KEY="owo"
 export CORS_ORIGINS="*"
 export API_PORT=":3000"
 export DB_CONNECTION="host=localhost user=libre_asi password=libre_asi dbname=libre_asi port=5432 sslmode=disable TimeZone=America/Bogota"
