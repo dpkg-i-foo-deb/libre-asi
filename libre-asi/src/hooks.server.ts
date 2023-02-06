962764cef8d5e473724fce1fc9ed0796ed977f96
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-06 17:41:26 -0500

    Redo try catch

--- libre-asi/src/hooks.server.ts
+++ libre-asi/src/hooks.server.ts
@@ -8,71 +8,70 @@ export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
 	request.headers.set('content-type', 'application/json');
 	let body = 'Cannot connect to the server';
 	let options = { status: 503, statusText: 'Cannot connect' };
+	let response: Response
 	const cookies = event.cookies;
-
 	try {
 		//TODO idk if this automatically forwards all the cookies
-		const response = await fetch(request);
-
-		const cookies = event.cookies;
-
-		if (
-			response.status == 401 &&
-			!request.url.includes('login') &&
-			!request.url.includes('refresh')
-		) {
-			const refresh = await fetch(API_URL + REFRESH, {
-				method: 'POST',
-				credentials: 'include',
-				headers: request.headers
-			});
-
-			if (refresh.ok) {
-				//Set the new access and refresh token as cookies
-				const newCookies = (await refresh.json()) as JwtPair;
-
-				console.log(newCookies.access_token);
-
-				cookies.set('access-token', newCookies.access_token.value, {
-					path: '/',
-					httpOnly: true
-				});
-				cookies.set('refresh-token', newCookies.refresh_token.value, {
-					path: '/',
-					httpOnly: true
-				});
-
-				//Make a new request and pray for it to work
-
-				const newRequest = request.clone();
-				newRequest.headers.set('cookie', 'access-token=' + newCookies.access_token.value);
-
-				const newResponse = await fetch(newRequest, {
-					credentials: 'include'
-				});
-
-				if (newResponse.ok) {
-					return newResponse;
-				}
-			}
-
-			cookies.set('access-token', '', { path: '/' });
-			cookies.set('refresh-token', '', { path: '/' });
-		}
-
-		if (response.status == 403) {
-			body = 'Not enough privileges';
-			options = { status: 403, statusText: 'Not enough privileges' };
-			return new Response(body, options);
-		}
-
-		return response;
+		response = await fetch(request);
 	} catch (e) {
 		console.error(e);
 		cookies.set('access-token', '', { path: '/' });
 		cookies.set('refresh-token', '', { path: '/' });
 		return new Response(body, options);
 	}
+
+	if (
+		response.status == 401 &&
+		!request.url.includes('login') &&
+		!request.url.includes('refresh')
+	) {
+		const refresh = await fetch(API_URL + REFRESH, {
+			method: 'POST',
+			credentials: 'include',
+			headers: request.headers
+		});
+
+		if (refresh.ok) {
+			//Set the new access and refresh token as cookies
+			const newCookies = (await refresh.json()) as JwtPair;
+
+			console.log(newCookies.access_token);
+
+			cookies.set('access-token', newCookies.access_token.value, {
+				path: '/',
+				httpOnly: true
+			});
+			cookies.set('refresh-token', newCookies.refresh_token.value, {
+				path: '/',
+				httpOnly: true
+			});
+
+			//Make a new request and pray for it to work
+
+			const newRequest = request.clone();
+			newRequest.headers.set('cookie', 'access-token=' + newCookies.access_token.value);
+
+			const newResponse = await fetch(newRequest, {
+				credentials: 'include'
+			});
+
+			if (newResponse.ok) {
+				return newResponse;
+			}
+		}
+
+		cookies.set('access-token', '', { path: '/' });
+		cookies.set('refresh-token', '', { path: '/' });
+	}
+
+	if (response.status == 403) {
+		body = 'Not enough privileges';
+		options = { status: 403, statusText: 'Not enough privileges' };
+		return new Response(body, options);
+	}
+
+	return response;
+
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
