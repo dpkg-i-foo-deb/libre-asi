e71dba00d305f92028e9575522411e6a3f0f3b15
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-06 18:27:37 -0500

    Add set-up store

---
+++ libre-asi/src/lib/stores/setupStore.ts
@@ -0,0 +1,23 @@
+import { browser } from "$app/environment"
+import { writable, type Writable } from "svelte/store"
+
+let isSetup = false
+
+let setup : Writable<boolean> = writable(isSetup)
+
+if (browser){
+    try{
+        isSetup = JSON.parse(localStorage.getItem('setup')?? JSON.stringify(isSetup))
+    }catch(e){
+        console.warn('Local storage setup is wrong, fixing...')
+    }
+    setup = writable(isSetup)
+}
+
+setup.subscribe(function(value:boolean){
+    if(browser){
+        localStorage.setItem('setup',JSON.stringify(value))
+    }
+})
+
+export default setup
