672dfecdc9903b84261d8adc745fa308b88704fd
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-07 14:00:51 -0500

    Remove unused stuffies

--- libre-asi/src/routes/api/administrators/+server.ts
+++ libre-asi/src/routes/api/administrators/+server.ts
@@ -3,9 +3,6 @@ import type Administrator from '$lib/models/Administrator';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async function ({ fetch, request }) {
-	const body = 'Success';
-	const options = { status: 200, statusText: 'Administrator Registered' };
-
 	const admin = (await request.json()) as Administrator;
 
 	const response = await fetch(API_URL + REGISTER_ADMIN, {
