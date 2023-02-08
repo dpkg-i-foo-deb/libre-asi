831859f775c13b51ac84382ff2b8daaabfb7338a
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-08 09:55:39 -0500

    Add another constant

--- libre-asi/src/lib/api/constants.ts
+++ libre-asi/src/lib/api/constants.ts
@@ -1,4 +1,5 @@
 const API_URL = 'http://desarrollo:3000/';
+const SET_UP = 'set-up';
 const INTERVIEWER_LOGIN = 'login/interviewer/';
 const ADMIN_LOGIN = 'login/admin';
 const SIGN_OUT = 'sign-out';
@@ -6,4 +7,13 @@ const REFRESH = 'refresh/';
 const GET_ADMINS = 'administrators/';
 const REGISTER_ADMIN = 'administrators/';
 
-export { API_URL, INTERVIEWER_LOGIN, ADMIN_LOGIN, SIGN_OUT, REFRESH, GET_ADMINS, REGISTER_ADMIN };
+export {
+	API_URL,
+	INTERVIEWER_LOGIN,
+	ADMIN_LOGIN,
+	SIGN_OUT,
+	REFRESH,
+	GET_ADMINS,
+	REGISTER_ADMIN,
+	SET_UP
+};
