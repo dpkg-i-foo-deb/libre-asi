075dc4c690d0b6fd7a3d126f382cc9a98e6b22cc
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-13 17:48:42 -0500

    typesafe-i18n experiments

--- libre-asi/src/routes/+layout.ts
+++ libre-asi/src/routes/+layout.ts
@@ -1,13 +1,13 @@
 import { setLocale } from '$lib/i18n/i18n-svelte';
 import { loadLocaleAsync } from '$lib/i18n/i18n-util.async';
-import type { LayoutLoad } from './$types';
+import { Locale } from '$lib/models/Locale';
 
-export const load = (async (event) => {
-	const locale = 'en';
+export const load = async (event) => {
+	const locale = Locale.EN;
 
 	await loadLocaleAsync(locale);
 
 	setLocale(locale);
 
 	return event.data;
-}) satisfies LayoutLoad;
+};
--- libre-asi/src/routes/+page.svelte
+++ libre-asi/src/routes/+page.svelte
@@ -2,20 +2,20 @@
 	import LL from '$lib/i18n/i18n-svelte';
 </script>
 
-<h1 class="title">{$LL.WELCOME_TITLE()}</h1>
+<h1 class="title">{$LL.welcome.TITLE()}</h1>
 
 <h4 class="subtitle">
-	{$LL.WELCOME_PARAGRAPH_1()}
+	{$LL.welcome.PARAGRAPH_1()}
 </h4>
 
-<h7 class="subtitle">{$LL.WELCOME_PARAGRAPH_2()}</h7>
+<h7 class="subtitle">{$LL.welcome.PARAGRAPH_2()}</h7>
 
 <p class="text">
-	{$LL.WELCOME_VISIT()}
+	{$LL.welcome.VISIT()}
 	<a target="_blank" rel="noreferrer" href="https://github.com/dpkg-i-foo-deb/libre-asi"
-		>{$LL.WELCOME_GITHUB_REPOSITORY()}
+		>{$LL.welcome.GITHUB_REPOSITORY()}
 	</a>
-	{$LL.WELCOME_KNOW()}
+	{$LL.welcome.KNOW()}
 </p>
 
 <style>
