10ce7b5724aea6e0d6b525fd1f8d145cc6240568
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-13 16:47:35 -0500

    Localize index page

--- libre-asi/src/routes/+page.svelte
+++ libre-asi/src/routes/+page.svelte
@@ -1,16 +1,21 @@
-<h1 class="title">Welcome to Libre-ASI</h1>
+<script lang="ts">
+	import LL from '$lib/i18n/i18n-svelte';
+</script>
+
+<h1 class="title">{$LL.WELCOME_TITLE()}</h1>
 
 <h4 class="subtitle">
-	Libre-ASI is a software tool to assist the interview process that involves the Addiction Severity
-	Index form
+	{$LL.WELCOME_PARAGRAPH_1()}
 </h4>
 
-<h7 class="subtitle">Use the session menu on the upper right corner to log in</h7>
+<h7 class="subtitle">{$LL.WELCOME_PARAGRAPH_2()}</h7>
 
 <p class="text">
-	Visit the <a target="_blank" rel="noreferrer" href="https://github.com/dpkg-i-foo-deb/libre-asi"
-		>Github repository
-	</a> to know more and read the documentation
+	{$LL.WELCOME_VISIT()}
+	<a target="_blank" rel="noreferrer" href="https://github.com/dpkg-i-foo-deb/libre-asi"
+		>{$LL.WELCOME_GITHUB_REPOSITORY()}
+	</a>
+	{$LL.WELCOME_KNOW()}
 </p>
 
 <style>
