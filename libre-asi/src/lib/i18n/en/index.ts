c4cff8055874a677c32f60f8fd042d88434ff15c
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-13 16:46:57 -0500

    i18n stuff

--- libre-asi/src/lib/i18n/en/index.ts
+++ libre-asi/src/lib/i18n/en/index.ts
@@ -1,8 +1,13 @@
 import type { BaseTranslation } from '../i18n-types';
 
 const en = {
-	// TODO: your translations go here
-	HI: 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n'
+	WELCOME_TITLE: 'Welcome to Libre-ASI',
+	WELCOME_PARAGRAPH_1:
+		'Libre-ASI is a software tool to assist the interview process that involves the Addiction Severity Index form',
+	WELCOME_PARAGRAPH_2: 'Use the session menu on the upper right corner to log in',
+	WELCOME_VISIT: 'Visit the',
+	WELCOME_GITHUB_REPOSITORY: 'GitHub repository',
+	WELCOME_KNOW: 'to know more and read the documentation'
 } satisfies BaseTranslation;
 
 export default en;
--- libre-asi/src/lib/models/Locale.ts
+++ libre-asi/src/lib/models/Locale.ts
@@ -1,4 +1,4 @@
 export enum Locale {
-	EN = 'EN',
-	ES = 'ES'
+	EN = 'en',
+	ES = 'es'
 }
