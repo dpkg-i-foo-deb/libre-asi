233863f8ef835da74812340ea30b90bc790d7f5e
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-14 10:24:36 -0500

    Fix locale persistance

--- libre-asi/src/routes/+layout.ts
+++ libre-asi/src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { setLocale } from '$lib/i18n/i18n-svelte';
 import { loadLocaleAsync } from '$lib/i18n/i18n-util.async';
 import { Locale } from '$lib/models/Locale';
+import locale from '$lib/stores/localeStore';
+import { get } from 'svelte/store';
 
 export const load = async (event) => {
 	await setUpLocale();
@@ -8,9 +10,20 @@ export const load = async (event) => {
 };
 
 async function setUpLocale() {
-	const locale = Locale.EN;
+	const storedLocale = get(locale);
 
-	await loadLocaleAsync(locale);
+	switch (storedLocale) {
+		case Locale.EN:
+			await loadLocaleAsync(Locale.EN);
+			setLocale(Locale.EN);
+			break;
+		case Locale.ES:
+			await loadLocaleAsync(Locale.ES);
+			setLocale(Locale.ES);
+			break;
 
-	setLocale(locale);
+		default:
+			await loadLocaleAsync(Locale.EN);
+			setLocale(Locale.EN);
+	}
 }
