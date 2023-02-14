223e0218bca5c98b940321447c3b5c30e919ef28
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-13 19:27:01 -0500

    Set locale on another function

--- libre-asi/src/routes/+layout.ts
+++ libre-asi/src/routes/+layout.ts
@@ -3,11 +3,14 @@ import { loadLocaleAsync } from '$lib/i18n/i18n-util.async';
 import { Locale } from '$lib/models/Locale';
 
 export const load = async (event) => {
-	const locale = Locale.ES;
+	await setUpLocale();
+	return event.data;
+};
+
+async function setUpLocale() {
+	const locale = Locale.EN;
 
 	await loadLocaleAsync(locale);
 
 	setLocale(locale);
-
-	return event.data;
-};
+}
