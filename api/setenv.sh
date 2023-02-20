eb6e69857278724c4d8f2ac5db7bc323581cd619
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-20 23:48:28 +0000

    Print stuff to console

--- api/setenv.sh
+++ api/setenv.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+echo "Setting up environment variables"
 export AUTH_KEY="owo"
 export CORS_ORIGINS="*"
 export API_PORT=":3000"
 export DB_CONNECTION="host=192.168.122.1 user=libre_asi password=libre_asi dbname=libre_asi port=5432 sslmode=disable TimeZone=America/Bogota"
 export PROD="false"
 export WORLD="false"
+echo "Setup complete"
