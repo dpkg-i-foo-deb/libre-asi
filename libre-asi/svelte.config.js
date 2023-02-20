bfa4dfdd4ec19ff4c1fb07f913f264eacf0d82b5
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-20 22:26:38 +0000

    Use adapter auto

--- libre-asi/svelte.config.js
+++ libre-asi/svelte.config.js
@@ -1,17 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), optimizeImports()],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), optimizeImports()],
 
-	kit: {
-		adapter: adapter({ out: 'build', fallback: 'index.html' }),
-		prerender: { entries: ['*'] }
-	}
+  kit: {
+    adapter: adapter(),
+  }
 };
 
 export default config;
