e7574220b150799658d78760ba23c4cb37594b8f
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-24 18:46:32 +0000

    More refactor

--- api/routes/register_route.go
+++ api/routes/register_route.go
@@ -1,11 +1,5 @@
 package routes
 
-import (
-	"libre-asi-api/app"
-	"libre-asi-api/auth"
-	"libre-asi-api/services"
-)
-
 func registerRoute() {
-	app.AddPost("/register/:role", auth.ValidateAccessToken, services.RegisterService)
+	//app.AddPost("/register/:role", auth.ValidateAccessToken, services.RegisterService)
 }
