ab0db36d452b46a362cd2670982a7341b281377d
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-25 00:18:41 +0000

    Show error message when server response is 500

--- libre-asi/src/lib/util/handleResponse.ts
+++ libre-asi/src/lib/util/handleResponse.ts
@@ -3,34 +3,35 @@ import setup from '$lib/stores/setupStore';
 import { sendError } from './notifications';
 
 export function handleResponse(code: number, allow401: boolean): boolean {
-	let shouldNavigate = true;
+  let shouldNavigate = true;
 
-	switch (code) {
-		//TODO not enough privileges page
-		case 403:
-			goto('/');
-			break;
+  switch (code) {
+    //TODO not enough privileges page
+    case 403:
+      goto('/');
+      break;
 
-		case 503:
-			goto('/cannot-connect');
-			setup.set(false);
-			break;
+    case 503:
+      goto('/cannot-connect');
+      setup.set(false);
+      break;
+    case 500:
+      sendError('Something went wrong', 'Internal server error')
+    case 412:
+      setup.set(false);
+      goto('/set-up');
+      break;
 
-		case 412:
-			setup.set(false);
-			goto('/set-up');
-			break;
+    case 401:
+      if (!allow401) {
+        sendError('Your session has expired', 'Log In again');
+        goto('/login');
+      }
+      break;
 
-		case 401:
-			if (!allow401) {
-				sendError('Your session has expired', 'Log In again');
-				goto('/login');
-			}
-			break;
+    default:
+      shouldNavigate = false;
+  }
 
-		default:
-			shouldNavigate = false;
-	}
-
-	return shouldNavigate;
+  return shouldNavigate;
 }
