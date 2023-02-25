ff4c9c1a37be694eb119a092b2150630f6696aef
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-25 02:43:06 +0000

    Format

--- libre-asi/src/lib/util/handleResponse.ts
+++ libre-asi/src/lib/util/handleResponse.ts
@@ -4,38 +4,39 @@ import { sendError } from './notifications';
 import { sendInfo } from './notifications';
 
 export function handleResponse(code: number, allow401: boolean): boolean {
-  let shouldNavigate = true;
+	let shouldNavigate = true;
 
-  switch (code) {
-    //TODO not enough privileges page
-    case 403:
-      goto('/');
-      break;
+	switch (code) {
+		//TODO not enough privileges page
+		case 403:
+			goto('/');
+			break;
 
-    case 503:
-      goto('/cannot-connect');
-      setup.set(false);
-      break;
-    case 500:
-      sendError('Something went wrong', 'Internal server error')
-      break;
-    case 412:
-      setup.set(false);
-      goto('/set-up');
-      break;
+		case 503:
+			goto('/cannot-connect');
+			setup.set(false);
+			break;
+		case 500:
+			sendError('Something went wrong', 'Internal server error');
+			break;
+		case 412:
+			setup.set(false);
+			goto('/set-up');
+			break;
 
-    case 401:
-      if (!allow401) {
-        sendError('Your session has expired', 'Log In again');
-        goto('/login');
-      }
-      break;
-    case 428:
-      sendInfo('You need to set a new password to continue', 'Set new password')
-      break;
-    default:
-      shouldNavigate = false;
-  }
+		case 401:
+			if (!allow401) {
+				sendError('Your session has expired', 'Log In again');
+				goto('/login');
+			}
+			break;
+		case 428:
+			sendInfo('You need to set a new password to continue', 'Set new password');
+			goto('/password-reset');
+			break;
+		default:
+			shouldNavigate = false;
+	}
 
-  return shouldNavigate;
+	return shouldNavigate;
 }
