7d9a213c3c62aefd7bfdb6efd0db36ecebfe9a99
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-26 02:37:52 +0000

    Validators

--- libre-asi/src/lib/util/formUtils.ts
+++ libre-asi/src/lib/util/formUtils.ts
@@ -1,30 +1,44 @@
 import emailValidator from './emailValidator';
 import emptyValidator from './emptyValidator';
+import matchValidator from './matchValidator';
 
 export function checkEmail(email: string): [caption: string, valid: boolean] {
-	if (!emptyValidator(email)) {
-		return ['Email is required', false];
-	}
+  if (!emptyValidator(email)) {
+    return ['Email is required', false];
+  }
 
-	if (!emailValidator(email)) {
-		return ['Enter a valid email', false];
-	}
+  if (!emailValidator(email)) {
+    return ['Enter a valid email', false];
+  }
 
-	return ['', true];
+  return ['', true];
 }
 
 export function checkUsername(username: string): [caption: string, valid: boolean] {
-	if (!emptyValidator(username)) {
-		return ['Username is required', false];
-	}
+  if (!emptyValidator(username)) {
+    return ['Username is required', false];
+  }
 
-	return ['', true];
+  return ['', true];
 }
 
 export function checkPassword(password: string): [caption: string, valid: boolean] {
-	if (!emptyValidator(password)) {
-		return ['Password is required', false];
-	}
+  if (!emptyValidator(password)) {
+    return ['Password is required', false];
+  }
 
-	return ['', true];
+  return ['', true];
+}
+
+export function checkPasswordConfirm(password: string, passwordConfirm: string): [caption: string, valid: boolean] {
+  if (!emptyValidator(password)) {
+    return ['Password confirmation is required', false]
+  }
+
+  if (!matchValidator(password, passwordConfirm)) {
+    return ['Passwords must match', false]
+  }
+
+  return ['', true]
+
 }
---
+++ libre-asi/src/lib/util/matchValidator.ts
@@ -0,0 +1,7 @@
+export default function matchValidator(value: string, valueCompare: string): boolean {
+  if (value != valueCompare) {
+    return false
+  }
+
+  return true
+}
