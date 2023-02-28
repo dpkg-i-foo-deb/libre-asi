076ccd3927763f911ada05a3ddd40921fa4cd108
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-02-28 02:27:47 +0000

    Service finished

--- api/services/set_password_service.go
+++ api/services/set_password_service.go
@@ -1,8 +1,37 @@
 package services
 
-import "libre-asi-api/models"
+import (
+	"libre-asi-api/database"
+	"libre-asi-api/errors"
+	"libre-asi-api/models"
+	"libre-asi-api/util"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func SetPasswordService(credentials models.PasswordChange, email string) error {
+
+	var user models.User
+
+	if database.DB.Where("email = ?", email).First(&user).Error != nil {
+		return errors.ErrAccessDenied
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(credentials.CurrentPassword)) != nil {
+		return errors.ErrAccessDenied
+	}
+
+	hashedPassword, err := util.HashPassword(credentials.NewPassword)
+
+	if err != nil {
+		return errors.ErrInternalError
+	}
+
+	user.Password = hashedPassword
+
+	if database.DB.Save(&user).Error != nil {
+		return errors.ErrInternalError
+	}
 
 	return nil
 }
