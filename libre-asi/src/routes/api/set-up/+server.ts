c626e9b358e0f8b184d55359ed6a8072f562fe41
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-04 19:21:47 -0500

    Remove setup POST

--- libre-asi/src/routes/api/set-up/+server.ts
+++ libre-asi/src/routes/api/set-up/+server.ts
@@ -1,21 +1,5 @@
 import { API_URL, SET_UP } from '$lib/api/constants';
-import type Administrator from '$lib/models/Administrator';
 import type { RequestHandler } from '@sveltejs/kit';
-
-export const POST: RequestHandler = async function ({ fetch, request }) {
-	const admin = (await request.json()) as Administrator;
-
-	const response = await fetch(API_URL + SET_UP, {
-		method: 'POST',
-		credentials: 'include',
-		headers: request.headers,
-		body: JSON.stringify(admin),
-    mode:'cors',
-
-	});
-
-	return response;
-};
 
 export const GET: RequestHandler = async function ({ fetch }) {
 	return await fetch(API_URL + SET_UP, { method: 'GET' });
