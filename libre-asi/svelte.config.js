f3c77090e54b5d25c4634efe8262bb25a5e41c1a
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-03-04 19:16:58 -0500

    Remove netlify dependencies

--- libre-asi/svelte.config.js
+++ libre-asi/svelte.config.js
@@ -1,19 +1,16 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), optimizeImports()],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), optimizeImports()],
 
-	kit: {
-		adapter: adapter({
-      edge:false,
-      split:true,
-   })
-	}
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
