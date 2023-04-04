fc5dccc6b4f467b8bcf1d02c37a92ac9019976ef
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-04 10:25:41 -0500

    Register patient service

--- api/services/patient_services.go
+++ api/services/patient_services.go
@@ -1,1 +1,49 @@
 package services
+
+import (
+	"libre-asi-api/database"
+	"libre-asi-api/errors"
+	"libre-asi-api/models"
+	"libre-asi-api/util"
+
+	"gorm.io/gorm"
+)
+
+func RegisterPatient(newPatient models.Patient) (*models.Patient, error) {
+
+	var p string
+	var found models.Patient
+	var err error
+
+	if err = database.DB.Where("email = ?", newPatient.Email).First(&found).Error; err != gorm.ErrRecordNotFound {
+		return nil, errors.ErrConflict
+	}
+
+	if database.DB.Where("username = ?", newPatient.Username).First(&found).Error != gorm.ErrRecordNotFound {
+		return nil, errors.ErrConflict
+	}
+
+	p, err = util.MakeRandomPassword()
+
+	if err != nil {
+		return nil, errors.ErrInternalError
+	}
+
+	newPatient.Password = p
+
+	newPatient.NeedsPasswordReset = true
+
+	newPatient.Password, err = util.HashPassword(newPatient.Password)
+
+	if err != nil {
+		return nil, errors.ErrInternalError
+	}
+
+	if err = database.DB.Create(&newPatient).Error; err != nil {
+		return nil, errors.ErrInternalError
+	}
+
+	newPatient.Password = p
+
+	return &newPatient, gorm.ErrNotImplemented
+}
