30f9ea0ef98ccf9fafb5e44cf1f74f280c1544bd
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-04 10:58:25 -0500

    Add more patient handlers

--- api/handlers/patient_handlers.go
+++ api/handlers/patient_handlers.go
@@ -5,6 +5,7 @@ import (
 	"libre-asi-api/models"
 	"libre-asi-api/services"
 	"libre-asi-api/util"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -24,6 +25,66 @@ func RegisterPatient(c *fiber.Ctx) error {
 	}
 
 	return c.Status(201).JSON(newPatient)
+}
 
-	return nil
+func GetPatients(c *fiber.Ctx) error {
+
+	patients, err := services.GetPatients()
+
+	if err != nil {
+		return util.HandleFiberError(c, err)
+	}
+
+	return c.Status(200).JSON(patients)
+}
+
+func GetPatient(c *fiber.Ctx) error {
+
+	id := c.Params("id")
+
+	idInt, err := strconv.Atoi(id)
+
+	if err != nil {
+		return util.HandleFiberError(c, errors.ErrBadRoute)
+	}
+
+	patient, err := services.GetPatient(uint(idInt))
+
+	if err != nil {
+		return util.HandleFiberError(c, err)
+	}
+
+	return c.Status(200).JSON(patient)
+}
+
+func UpdatePatient(c *fiber.Ctx) error {
+
+	var updatedPatient *models.Patient
+
+	if c.BodyParser(&updatedPatient) != nil {
+		return util.HandleFiberError(c, errors.ErrCheckRequest)
+	}
+
+	if err := services.UpdatePatient(*updatedPatient); err != nil {
+		return util.HandleFiberError(c, err)
+	}
+
+	return util.SendSuccess(c, 200, "Updated")
+}
+
+func DeletePatient(c *fiber.Ctx) error {
+
+	id := c.Params("id")
+
+	idInt, err := strconv.Atoi(id)
+
+	if err != nil {
+		return util.HandleFiberError(c, errors.ErrBadRoute)
+	}
+
+	if err := services.DeletePatient(uint(idInt)); err != nil {
+		return util.HandleFiberError(c, err)
+	}
+
+	return util.SendSuccess(c, 200, "Deleted")
 }
