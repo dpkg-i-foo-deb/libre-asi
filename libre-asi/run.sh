70cef9e455b8263c37033bbffd0571e4661a09f4
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-06 16:09:33 -0500

    Update scripts

--- libre-asi/run.sh
+++ libre-asi/run.sh
@@ -4,6 +4,11 @@
 if [ "$1" == "dev" ]; then
   # set the prerender option to false
   export VITE_PRERENDER=false
+  # check if the files cert.perm and key.perm exist in the current directory
+  if [ ! -f cert.pem ] || [ ! -f key.pem ]; then
+    # run the openssl command to generate them
+    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
+  fi
 elif [ "$1" == "prod" ]; then
   # set the prerender option to true
   export VITE_PRERENDER=true
