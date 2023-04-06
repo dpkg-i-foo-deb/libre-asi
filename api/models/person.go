3554bb6d27fcf1dad81d430701f3cc91965dba54
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-05 19:05:59 -0500

    Use inheritance correctly

--- api/models/person.go
+++ api/models/person.go
@@ -8,13 +8,16 @@ import (
 
 type Person struct {
 	gorm.Model
-	FirstName    string    `json:"firstName" gorm:"not null;default:null"`
-	LastName     string    `json:"lastName"`
-	FirstSurname string    `json:"firstSurname"`
-	LastSurname  string    `json:"lastSurname"`
-	Birthdate    time.Time `json:"birthdate"`
-	Age          int       `json:"age"`
-	PersonalID   string    `json:"personalID"`
-	Addresses    []Address `json:"addresses"`
-	Phones       []Phone   `json:"phones"`
+	FirstName    string        `json:"firstName" gorm:"not null;default:null"`
+	LastName     string        `json:"lastName"`
+	FirstSurname string        `json:"firstSurname"`
+	LastSurname  string        `json:"lastSurname"`
+	Birthdate    time.Time     `json:"birthdate"`
+	Age          int           `json:"age"`
+	PersonalID   string        `json:"personalID"`
+	Addresses    []Address     `json:"addresses"`
+	Phones       []Phone       `json:"phones" gorm:"foreignkey:PersonID;association_foreignkey:ID"`
+	Interviewers []Interviewer `json:"interviewers" gorm:"foreignKey:PersonID"`
+	Patients     []Patient     `json:"patients" gorm:"foreignKey:PersonID"`
+	UserID       uint          `json:"userID" gorm:"not null;default:null"`
 }
