3644d7c07f9a07036ab5a1ea81ceb2b62e62f104
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-10 18:39:42 -0500

    Redesign patient creation page

--- libre-asi/src/routes/management/patients/create/+page.svelte
+++ libre-asi/src/routes/management/patients/create/+page.svelte
@@ -4,10 +4,15 @@
 		DatePickerInput,
 		TextInput,
 		Tile,
-		Toggle,
 		FileUploader,
 		Button,
-		ButtonSet
+		ButtonSet,
+		Tabs,
+		Tab,
+		TabContent,
+		ProgressIndicator,
+		ProgressStep,
+		Toggle
 	} from 'carbon-components-svelte';
 	import { goto } from '$app/navigation';
 	import type Patient from '$lib/models/Patient';
@@ -16,7 +21,6 @@
 	import { sendSuccess } from '$lib/util/notifications';
 	import { handleResponse } from '$lib/util/handleResponse';
 
-	let showInfo = false;
 	let file: File | null = null;
 	let imageUrl: string | null = null;
 	let files: File[] = [];
@@ -74,167 +78,164 @@
 </script>
 
 <main>
-	<div class="wrapper">
-		<div class="toggle">
-			<Toggle labelText="Mostrar información adicional" bind:toggled={showInfo} />
+	<div class="form">
+		<div class="title">
+			<h1>Creando un nuevo paciente</h1>
 		</div>
 
-		<div class="form">
-			<div class="title">
-				<h1>Crear un nuevo paciente</h1>
-			</div>
+		<Tabs type="container">
+			<Tab label="Datos básicos" />
+			<Tab label="Datos extra" />
 
-			<Tile>
-				<div class="basic-data">
-					<h3>Datos básicos</h3>
-					<div class="subtitle">
-						<p class="bold">Estos son los datos mínimos para registrar un paciente</p>
-						<div class="image-container" style="display: flex; margin-top: 20px; padding: 70px;">
-							<div
-								class="file-uploader-container"
-								style="width: 100px; height: 100px; margin-right: 50px; padding: 5px; position: relative;"
-							>
-								<FileUploader
-									bind:files
-									on:add={handleFileSelect}
-									on:remove={handleFileRemove}
-									multiple={false}
-									labelTitle="Upload files"
-									buttonLabel="Add Picture"
-									labelDescription="Only JPEG files are accepted."
-									accept={['.jpg', '.jpeg']}
-									status="complete"
-									style="font-size: 10px;"
-								/>
-							</div>
-
-							<div
-								class="image-preview-container"
-								style="width: 100px; height: 100px; margin-left: 20px; padding: 10px; border: 1px solid black; position: relative;"
-							>
-								{#if imageUrl}
-									<div style="position: relative;">
-										<!-- svelte-ignore a11y-img-redundant-alt -->
-										<img
-											src={imageUrl}
-											alt="Profile Picture"
-											style="width: 100%; height: 100%; object-fit: cover; margin-bottom: 10px;"
-										/>
-										<div
-											style="position: absolute; bottom: 0; left: 0; right: 0; text-align: center;"
-										>
-											Profile Picture
-										</div>
-										{#if imageUrl}
-											<div
-												style="position: absolute; bottom: -50px; left: 0; right: 0; text-align: center;"
-											>
-												<Button kind="danger" on:click={removeImage} style="margin: auto;"
-													>Remove</Button
-												>
-											</div>
-										{/if}
-									</div>
-								{/if}
-							</div>
-						</div>
-						<div class="text-field">
-							<TextInput
-								id="firstName"
-								labelText="Primer Nombre"
-								placeholder="Ingresa el primer nombre"
-								bind:value={newPatient.firstName}
-							/>
-						</div>
-
-						<div class="text-field">
-							<TextInput
-								id="id"
-								labelText="Número de Identificación"
-								placeholder="Ingresa el número de identificación"
-								bind:value={newPatient.personalID}
-							/>
-						</div>
-
-						<div class="text-field">
-							<TextInput
-								style="margin-bottom: 3rem"
-								id="email"
-								type="email"
-								labelText="Correo Electrónico"
-								placeholder="Ingresa el correo Electrónico"
-								bind:value={newPatient.email}
-							/>
-						</div>
-					</div>
-
-					{#if showInfo}
-						<div class="other-data">
-							<h3>Otros datos</h3>
-							<div class="subtitle">
-								<p class="bold">
-									Datos extra que pueden servir para una mejor identificación del paciente
-								</p>
-							</div>
-							<div class="text-field">
-								<TextInput
-									id="secondname"
-									labelText="Segundo Nombre"
-									placeholder="Ingresa el segundo nombre"
-									bind:value={newPatient.lastName}
-								/>
-							</div>
-
-							<div class="text-field">
-								<TextInput
-									id="firstSurname"
-									labelText="Primer Apellido"
-									placeholder="Ingresa el primer apellido"
-									bind:value={newPatient.firstSurname}
-								/>
-							</div>
-
-							<div class="text-field">
-								<TextInput
-									id="secondSurname"
-									labelText="Segundo Apellido"
-									placeholder="Ingrese el segundo apellido"
-									bind:value={newPatient.lastSurname}
-								/>
-							</div>
-
-							<div class="text-field">
-								<DatePicker datePickerType="single" dateFormat="d/m/Y" on:change>
-									<DatePickerInput
-										labelText="Fecha de nacimiento"
-										placeholder="dd/mm/aaaa"
-										bind:value={newPatient.birthdate}
+			<svelte:fragment slot="content">
+				<TabContent>
+					<div class="basic-data">
+						<div class="subtitle">
+							<p class="bold">Estos son los datos mínimos para registrar un paciente</p>
+							<div class="image-container" style="display: flex; margin-top: 20px; padding: 70px;">
+								<div
+									class="file-uploader-container"
+									style="width: 100px; height: 100px; margin-right: 50px; padding: 5px; position: relative;"
+								>
+									<FileUploader
+										bind:files
+										on:add={handleFileSelect}
+										on:remove={handleFileRemove}
+										multiple={false}
+										labelTitle="Upload files"
+										buttonLabel="Add Picture"
+										labelDescription="Only JPEG files are accepted."
+										accept={['.jpg', '.jpeg']}
+										status="complete"
+										style="font-size: 10px;"
 									/>
-								</DatePicker>
+								</div>
+
+								<div
+									class="image-preview-container"
+									style="width: 100px; height: 100px; margin-left: 20px; padding: 10px; border: 1px solid black; position: relative;"
+								>
+									{#if imageUrl}
+										<div style="position: relative;">
+											<!-- svelte-ignore a11y-img-redundant-alt -->
+											<img
+												src={imageUrl}
+												alt="Profile Picture"
+												style="width: 100%; height: 100%; object-fit: cover; margin-bottom: 10px;"
+											/>
+											<div
+												style="position: absolute; bottom: 0; left: 0; right: 0; text-align: center;"
+											>
+												Profile Picture
+											</div>
+											{#if imageUrl}
+												<div
+													style="position: absolute; bottom: -50px; left: 0; right: 0; text-align: center;"
+												>
+													<Button kind="danger" on:click={removeImage} style="margin: auto;"
+														>Remove</Button
+													>
+												</div>
+											{/if}
+										</div>
+									{/if}
+								</div>
+							</div>
+							<div class="text-field">
+								<TextInput
+									id="firstName"
+									labelText="Primer Nombre"
+									placeholder="Ingresa el primer nombre"
+									bind:value={newPatient.firstName}
+								/>
+							</div>
+
+							<div class="text-field">
+								<TextInput
+									id="id"
+									labelText="Número de Identificación"
+									placeholder="Ingresa el número de identificación"
+									bind:value={newPatient.personalID}
+								/>
+							</div>
+
+							<div class="text-field">
+								<TextInput
+									style="margin-bottom: 3rem"
+									id="email"
+									type="email"
+									labelText="Correo Electrónico"
+									placeholder="Ingresa el correo Electrónico"
+									bind:value={newPatient.email}
+								/>
 							</div>
 						</div>
-					{/if}
-					<div id="register-button">
-						<ButtonSet>
-							<Button kind="secondary">Cancel</Button>
-							<Button
-								href=""
-								on:click={() => {
-									handleRegisterPatient();
-								}}>Register Patient</Button
-							>
-						</ButtonSet>
 					</div>
-				</div>
-			</Tile>
+				</TabContent>
+				<TabContent
+					><div class="other-data">
+						<div class="subtitle">
+							<p class="bold">
+								Datos extra que pueden servir para una mejor identificación del paciente
+							</p>
+						</div>
+						<div class="text-field">
+							<TextInput
+								id="secondname"
+								labelText="Segundo Nombre"
+								placeholder="Ingresa el segundo nombre"
+								bind:value={newPatient.lastName}
+							/>
+						</div>
+
+						<div class="text-field">
+							<TextInput
+								id="firstSurname"
+								labelText="Primer Apellido"
+								placeholder="Ingresa el primer apellido"
+								bind:value={newPatient.firstSurname}
+							/>
+						</div>
+
+						<div class="text-field">
+							<TextInput
+								id="secondSurname"
+								labelText="Segundo Apellido"
+								placeholder="Ingrese el segundo apellido"
+								bind:value={newPatient.lastSurname}
+							/>
+						</div>
+
+						<div class="text-field">
+							<DatePicker datePickerType="single" dateFormat="d/m/Y" on:change>
+								<DatePickerInput
+									labelText="Fecha de nacimiento"
+									placeholder="dd/mm/aaaa"
+									bind:value={newPatient.birthdate}
+								/>
+							</DatePicker>
+						</div>
+					</div>
+				</TabContent>
+			</svelte:fragment>
+		</Tabs>
+		<div id="register-button">
+			<ButtonSet>
+				<Button kind="secondary">Cancelar</Button>
+				<Button
+					href=""
+					on:click={() => {
+						handleRegisterPatient();
+					}}>Registrar Paciente</Button
+				>
+			</ButtonSet>
 		</div>
 	</div>
 </main>
 
 <style>
 	.other-data {
-		margin-top: 3rem;
-		margin-bottom: 5rem;
+		margin-bottom: 3rem;
 	}
 
 	.basic-data {
@@ -255,18 +256,6 @@
 
 	.text-field {
 		margin-top: 3rem;
-	}
-
-	.wrapper {
-		display: grid;
-		grid-template-columns: 10% 75%;
-		grid-gap: 50px;
-		grid-auto-rows: minmax(100px, auto);
-	}
-
-	.toggle {
-		grid-column: 1;
-		grid-row: 1 / 5;
 	}
 
 	.form {
