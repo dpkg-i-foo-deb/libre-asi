59645a7bada0169ffb22fd9f625cb766ba3071e4
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-11 10:46:15 -0500

    First interviewer create page

--- libre-asi/src/routes/management/interviewers/create/+page.svelte
+++ libre-asi/src/routes/management/interviewers/create/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import type Interviewer from '$lib/models/Interviewer';
+	import { Column, Grid, Row, Tile } from 'carbon-components-svelte';
+
+	let file: File | null = null;
+	let imageUrl: string | null = null;
+	let files: File[] = [];
 
 	let newInterviewer: Interviewer = {
 		email: '',
@@ -13,8 +18,39 @@
 		personalID: '',
 		age: 0
 	};
+
+	function handleFileSelect(event: CustomEvent) {
+		const selectedFiles = event.detail;
+		if (selectedFiles && selectedFiles.length > 0) {
+			file = selectedFiles[0];
+			if (file) {
+				imageUrl = URL.createObjectURL(file);
+			}
+		}
+		files = [];
+	}
+	function removeImage() {
+		imageUrl = null;
+		files = [];
+	}
+
+	function handleFileRemove(event: CustomEvent<readonly File[]>) {
+		const file = event.detail[0];
+		if (file === files[0]) {
+			imageUrl = null;
+			files = [];
+		}
+	}
 </script>
 
-<main>olis</main>
+<main>
+	<Tile>
+		<Grid>
+			<Row>
+				<Column />
+			</Row>
+		</Grid>
+	</Tile>
+</main>
 
 <style></style>
