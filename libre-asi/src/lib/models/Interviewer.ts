dc0fb2d4395f05011a8820ec3d3b02ba5945b13a
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-11 10:16:07 -0500

    Format

--- libre-asi/src/lib/models/Interviewer.ts
+++ libre-asi/src/lib/models/Interviewer.ts
@@ -1,19 +1,17 @@
 interface Interviewer {
-    ID?: number;
-    CreatedAt?: Date;
-    UpdatedAt?: Date;
-    email: string;
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    firstSurName: string;
-    lastSurName: string;
-    birthdate: Date;
-    age: number;
-    personalId: string;
-
-
+	ID?: number;
+	CreatedAt?: Date;
+	UpdatedAt?: Date;
+	email: string;
+	username: string;
+	password: string;
+	firstName: string;
+	lastName: string;
+	firstSurName: string;
+	lastSurName: string;
+	birthdate: Date;
+	age: number;
+	personalId: string;
 }
 
 export default Interviewer;
