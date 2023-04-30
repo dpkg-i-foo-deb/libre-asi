5a9599d5f3ff4cc35525c645ba655a8e219e9e14
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-04-30 11:59:56 -0500

    Remove blob

--- api/pkg/models/person.go
+++ api/pkg/models/person.go
@@ -15,7 +15,7 @@ type Person struct {
 	Birthdate    time.Time     `json:"birthdate"`
 	Age          int           `json:"age"`
 	PersonalID   string        `json:"personalID"`
-	Picture      []byte        `json:"picture" gorm:"type:blob"`
+	Picture      []byte        `json:"picture"`
 	Addresses    []Address     `json:"addresses"`
 	Phones       []Phone       `json:"phones" gorm:"foreignkey:PersonID;association_foreignkey:ID"`
 	Interviewers []Interviewer `json:"interviewers" gorm:"foreignKey:PersonID"`
