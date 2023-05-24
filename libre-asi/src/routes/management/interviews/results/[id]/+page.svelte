1966a76e38e209359152dcd29e89178adfe5360a
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-05-24 04:28:55 -0500

    Add results

--- libre-asi/src/routes/management/interviews/results/[id]/+page.svelte
+++ libre-asi/src/routes/management/interviews/results/[id]/+page.svelte
@@ -4,7 +4,12 @@
 	import type Interview from '$lib/models/Interview';
 	import { fetchWithRefresh } from '$lib/util/fetchRefresh';
 	import { handleResponse } from '$lib/util/handleResponse';
-	import { InlineLoading } from 'carbon-components-svelte';
+	import {
+		DataTable,
+		DataTableSkeleton,
+		InlineLoading,
+		SkeletonText
+	} from 'carbon-components-svelte';
 	import type { Result } from '$lib/models/Result';
 	import { onMount } from 'svelte';
 
@@ -41,9 +46,82 @@
 </script>
 
 <main>
-	{#if loading}
-		<InlineLoading description="Computando resultados..." />
+	{#if loading || !result}
+		<div class="load">
+			<InlineLoading description="Computando resultados..." />
+		</div>
+
+		<div class="title">
+			<SkeletonText heading />
+		</div>
+
+		<DataTableSkeleton showHeader={false} showToolbar={false} />
+	{:else}
+		<h1 class="title">Resultados</h1>
+
+		<DataTable
+			headers={[
+				{ key: 'scale', value: 'Índices de Severidad' },
+				{ key: 'value', value: 'Valor' }
+			]}
+			rows={[
+				{
+					id: 'med',
+					scale: 'Médico',
+					value: result.medicalScale
+				},
+				{
+					id: 'emp',
+					scale: 'Empleo',
+					value: result.employmentScale
+				},
+				{
+					id: 'alc',
+					scale: 'Alcohol',
+					value: result.alcoholScale
+				},
+				{
+					id: 'dru',
+					scale: 'Drogas',
+					value: result.drugScale
+				},
+				{
+					id: 'law',
+					scale: 'Legal',
+					value: result.legalScale
+				},
+				{
+					id: 'famSupport',
+					scale: 'Familia (Apoyo)',
+					value: result.familySocialSupportScale
+				},
+				{
+					id: 'famChild',
+					scale: 'Familia (Niños)',
+					value: result.familyChildScale
+				},
+				{
+					id: 'famProb',
+					scale: 'Familia (Problemas)',
+					value: result.familySocialProblemScale
+				},
+				{
+					id: 'psy',
+					scale: 'Psicológico',
+					value: result.psychScale
+				}
+			]}
+		/>
 	{/if}
 </main>
 
-<style></style>
+<style>
+	.load {
+		margin-bottom: 2rem;
+	}
+
+	.title {
+		margin-top: 1.5rem;
+		margin-bottom: 1.5rem;
+	}
+</style>
