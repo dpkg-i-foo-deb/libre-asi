bebe8ba9abf5fbf14d5afc94998a8242e1598809
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-05-27 10:09:38 -0500

    Fix start interview service and handler

--- api/cmd/handlers/interview_handlers.go
+++ api/cmd/handlers/interview_handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"libre-asi-api/pkg/auth"
 	"libre-asi-api/pkg/errors"
 	"libre-asi-api/pkg/services"
 	"libre-asi-api/pkg/util"
@@ -21,7 +22,15 @@ func StartInterview(c *fiber.Ctx) error {
 		return util.HandleFiberError(c, errors.ErrCheckRequest)
 	}
 
-	i, err = services.StartInterview(int(i.PatientID), int(i.InterviewerID))
+	tk := c.Cookies("access-token")
+
+	email, err := auth.EmailFromToken(tk)
+
+	if err != nil {
+		return util.HandleFiberError(c, errors.ErrAccessDenied)
+	}
+
+	i, err = services.StartInterview(int(i.PatientID), email)
 
 	if err != nil {
 		return util.HandleFiberError(c, err)
