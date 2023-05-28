79eb781e2f0e87f42a4772a88413deb4ed9304b6
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2023-05-28 10:21:31 -0500

    Remove server function

--- libre-asi/src/routes/api/set-up/+server.ts
+++
@@ -1,6 +0,0 @@
-import { API_URL, SET_UP } from '$lib/api/constants';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async function ({ fetch }) {
-	return await fetch(API_URL + SET_UP, { method: 'GET' });
-};
